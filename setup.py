2e2dd250359ded7529a533773575f2bf0b71a580
Leighton Pritchard <leighton.pritchard@mac.com>
2020-07-21 15:37:20 +0100

    fix setup.py quote issue

--- setup.py
+++ setup.py
@@ -1,3 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) The James Hutton Institute 2017-2019
+# (c) University of Strathclyde 2019-2020
+# Author: Leighton Pritchard
+#
+# Contact:
+# leighton.pritchard@strath.ac.uk
+#
+# Leighton Pritchard,
+# Strathclyde Institute for Pharmacy and Biomedical Sciences,
+# Cathedral Street,
+# Glasgow,
+# G1 1XQ
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Copyright (c) 2017-2019 The James Hutton Institute
+# Copyright (c) 2019-2020 University of Strathclyde
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Distribution setup."""
+
+import sys
+import re
+
+from pathlib import Path
+
 # try using distribute or setuptools or distutils.
 try:
     import distribute_setup
@@ -6,27 +52,21 @@ try:
 except ImportError:
     pass
 
-import setuptools
-
-import os
-import sys
-import re
-
 # Get long description from README.md
-with open("README.md", "r") as dfh:
-    long_description = dfh.read()
+with Path("README.md").open("r") as dfh:
+    long_description = dfh.read()  # pyline: disable=C0103
 
 # parse version from package/module without importing or evaluating the code
-vfname = os.path.join("ncbi_cds_from_protein", "__init__.py")
-with open(vfname, "r") as fh:
-    for line in fh:
-        m = re.search(r"^__version__ = '(?P<version>[^']+)'$", line)
-        if m:
-            version = m.group("version")
+with Path("ncbi_cds_from_protein/__init__.py").open("r") as ifh:
+    for line in ifh:
+        print(line)
+        match = re.search(r'^__version__ = "(?P<version>[^"]+)"$', line)
+        if match:
+            version = match.group("version")
             break
 
-if sys.version_info < (3, 5):
-    sys.stderr.write("ERROR: ncfp requires Python 3.5 " + "or above...exiting.\n")
+if sys.version_info <= (3, 6):
+    sys.stderr.write("ERROR: ncfp requires Python 3.7 or above (exiting).\n")
     sys.exit(1)
 
 setuptools.setup(
@@ -53,9 +93,7 @@ setuptools.setup(
     include_package_date=True,
     install_requires=["biopython", "tqdm"],
     python_requires="~=3.5",
-    entry_points={
-        "console_scripts": ["ncfp = ncbi_cds_from_protein.scripts.ncfp:run_main"]
-    },
+    entry_points={"console_scripts": ["ncfp = ncbi_cds_from_protein.scripts.ncfp:run_main"]},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
@@ -66,8 +104,8 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
