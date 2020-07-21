c1164c6624d23c21a63a0f4b2a6001d885126863
Leighton Pritchard <leighton.pritchard@mac.com>
2020-07-21 16:40:40 +0100

    convert tests for basic pytest runs
    This revealed a bit of an issue. Something has changed on the NCBI
    side, and the old method of recovering coding sequences is broken
    somehow. This would have been noticed if I'd have run periodic
    regression tests against the remote service (but I've not been
    doing). This needs to be fixed.

--- tests/test_cli_parsing.py
+++ tests/test_cli_parsing.py
@@ -1,60 +1,51 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""test_cli_parsing.py
-
-Test command-line parsing for ncfp program
-
-This test suite is intended to be run from the repository root using:
-
-nosetests -v
-
-(c) The James Hutton Institute 2018
-Author: Leighton Pritchard
-
-Contact:
-leighton.pritchard@hutton.ac.uk
-
-Leighton Pritchard,
-Information and Computing Sciences,
-James Hutton Institute,
-Errol Road,
-Invergowrie,
-Dundee,
-DD6 9LH,
-Scotland,
-UK
-
-The MIT License
-
-Copyright (c) 2017 The James Hutton Institute
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
+# (c) The James Hutton Institute 2017-2019
+# (c) University of Strathclyde 2019-2020
+# Author: Leighton Pritchard
+#
+# Contact:
+# leighton.pritchard@strath.ac.uk
+#
+# Leighton Pritchard,
+# Strathclyde Institute for Pharmacy and Biomedical Sciences,
+# Cathedral Street,
+# Glasgow,
+# G1 1XQ
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Copyright (c) 2017-2019 The James Hutton Institute
+# Copyright (c) 2019-2020 University of Strathclyde
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Test command-line parsing for ncfp program"""
 
 import logging
 import os
 import unittest
 
-from nose.tools import (raises,)
+import pytest
 
-from ncbi_cds_from_protein.scripts import (ncfp,)
-from ncbi_cds_from_protein.ncfp_tools import (NCFPException,)
+from ncbi_cds_from_protein.scripts import ncfp
+from ncbi_cds_from_protein.ncfp_tools import NCFPException
 
 
 class TestCLIParsing(unittest.TestCase):
@@ -62,31 +53,38 @@ class TestCLIParsing(unittest.TestCase):
 
     def setUp(self):
         """Set attributes for tests."""
-        self.indir = os.path.join('tests', 'test_input', 'sequences')
-        self.outdir = os.path.join('tests', 'test_output', 'parsertests')
-        self.email = 'ncfptest@dev.null'
+        self.indir = os.path.join("tests", "test_input", "sequences")
+        self.outdir = os.path.join("tests", "test_output", "parsertests")
+        self.email = "ncfptest@dev.null"
         # Lists of command-line arguments for each test
-        self.argsdict = {'validate_and_log': [os.path.join(self.indir,
-                                                           'input_uniprot_stockholm_small.fasta'),
-                                              self.outdir,
-                                              self.email,
-                                              '--uniprot', '--stockholm',
-                                              '--disabletqdm',
-                                              '-l', os.path.join('tests', 'test_output', 'parsertests',
-                                                                 'download_logfile.log'), ],
-                         'bad_infile': [os.path.join('tests', 'test_input',
-                                                     'sequences', 'notexist.fasta'),
-                                        self.outdir,
-                                        self.email,
-                                        '--disabletqdm', ],
-                         'local_cache': [os.path.join(self.indir, 'human.fasta'),
-                                         self.outdir,
-                                         self.email,
-                                         '--disabletqdm',
-                                         '-d', os.path.join(self.outdir,
-                                                            'cache'),
-                                         '-c', 'humancache', ],
-                         }
+        self.argsdict = {
+            "validate_and_log": [
+                os.path.join(self.indir, "input_uniprot_stockholm_small.fasta"),
+                self.outdir,
+                self.email,
+                "--uniprot",
+                "--stockholm",
+                "--disabletqdm",
+                "-l",
+                os.path.join("tests", "test_output", "parsertests", "download_logfile.log"),
+            ],
+            "bad_infile": [
+                os.path.join("tests", "test_input", "sequences", "notexist.fasta"),
+                self.outdir,
+                self.email,
+                "--disabletqdm",
+            ],
+            "local_cache": [
+                os.path.join(self.indir, "human.fasta"),
+                self.outdir,
+                self.email,
+                "--disabletqdm",
+                "-d",
+                os.path.join(self.outdir, "cache"),
+                "-c",
+                "humancache",
+            ],
+        }
 
         # Null logger for testing
         self.logger = logging.getLogger("TestCLIParsing logger")
@@ -94,21 +92,17 @@ class TestCLIParsing(unittest.TestCase):
 
     def test_download_and_log(self):
         """ncfp downloads coding sequences and logs output from CLI."""
-        ncfp.run_main(self.argsdict['validate_and_log'],
-                      logger=self.logger)
+        ncfp.run_main(self.argsdict["validate_and_log"], logger=self.logger)
 
-    @raises(NCFPException)
     def test_bad_infile(self):
         """ncfp stops if CLI input file does not exist."""
-        ncfp.run_main(self.argsdict['bad_infile'],
-                      logger=self.logger)
+        with pytest.raises(NCFPException):
+            ncfp.run_main(self.argsdict["bad_infile"], logger=self.logger)
 
     def test_create_and_keep_cache(self):
         """ncfp creates named cache from CLI and keeps it when rerunning."""
         self.logger.info("Creating local cache")
-        ncfp.run_main(self.argsdict['local_cache'],
-                      logger=self.logger)
+        ncfp.run_main(self.argsdict["local_cache"], logger=self.logger)
 
         self.logger.info("Reusing local cache")
-        ncfp.run_main(self.argsdict['local_cache'] + ['--keepcache'],
-                      logger=self.logger)
+        ncfp.run_main(self.argsdict["local_cache"] + ["--keepcache"], logger=self.logger)
--- tests/test_ncfp.py
+++ tests/test_ncfp.py
@@ -1,58 +1,42 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""test_ncfp.py
-
-Test commandline entry for the ncfp program.
-
-This test suite is intended to be run from the repository root using:
-
-nosetests -v
-
-For each test, command-line options are defined in a Namespace,
-and passed as the sole argument to the run_main() function in
-ncfp.py.
-
-Paths are defined relative to the repository root, where the tests
-are expected to be run.
-
-(c) The James Hutton Institute 2017
-Author: Leighton Pritchard
-
-Contact:
-leighton.pritchard@hutton.ac.uk
-
-Leighton Pritchard,
-Information and Computing Sciences,
-James Hutton Institute,
-Errol Road,
-Invergowrie,
-Dundee,
-DD6 9LH,
-Scotland,
-UK
-
-The MIT License
-
-Copyright (c) 2017 The James Hutton Institute
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
+# (c) The James Hutton Institute 2017-2019
+# (c) University of Strathclyde 2019-2020
+# Author: Leighton Pritchard
+#
+# Contact:
+# leighton.pritchard@strath.ac.uk
+#
+# Leighton Pritchard,
+# Strathclyde Institute for Pharmacy and Biomedical Sciences,
+# Cathedral Street,
+# Glasgow,
+# G1 1XQ
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Copyright (c) 2017-2019 The James Hutton Institute
+# Copyright (c) 2019-2020 University of Strathclyde
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Test commandline entry for the ncfp program."""
 
 import logging
 import os
@@ -60,8 +44,6 @@ import time
 import unittest
 
 from argparse import Namespace
-
-from nose.tools import assert_equal
 
 from ncbi_cds_from_protein.scripts import ncfp
 
@@ -127,18 +109,14 @@ class TestBasicUse(unittest.TestCase):
         ncfp.run_main(namespace, self.logger)
 
         # Compare output
-        self.check_files(
-            outdirname, ("ncfp_aa.fasta", "ncfp_nt.fasta", "skipped.fasta")
-        )
+        self.check_files(outdirname, ("ncfp_aa.fasta", "ncfp_nt.fasta", "skipped.fasta"))
 
     def test_basic_stockholm(self):
         """ncfp collects correct coding sequences for basic UniProt/Stockholm input."""
         # Modify default arguments
         outdirname = "small_stockholm"  # common dirname for output,target
         namespace = self.base_namespace
-        namespace.infname = os.path.join(
-            self.datadir, "input_uniprot_stockholm_small.fasta"
-        )
+        namespace.infname = os.path.join(self.datadir, "input_uniprot_stockholm_small.fasta")
         namespace.outdirname = os.path.join(self.outdir, outdirname)
         namespace.uniprot = True
         namespace.stockholm = True
@@ -154,4 +132,4 @@ class TestBasicUse(unittest.TestCase):
         for fname in fnames:
             with open(os.path.join(self.outdir, outdirname, fname)) as fh1:
                 with open(os.path.join(self.targetdir, outdirname, fname)) as fh2:
-                    assert_equal(fh1.read(), fh2.read())
+                    assert fh1.read() == fh2.read()
